e5cc811072ffad4b24c5f43f324e1d66b0950918
mrNikitoss <nikita.d.timofeev@yandex.ru>
2023-05-07 17:23:28 +0300

    Обновление проетка. Выполнил Модуль 1. Не могу выполнить Модуль 2

--- DjangoCountries/urls.py
+++ DjangoCountries/urls.py
@@ -5,5 +5,8 @@ from MainApp import views
 urlpatterns = [
     path('', views.home),
     path('about/', views.about),
+    path('countries_list/', views.countries_list),
+    path('country/<int:id>/', views.country_page),
+    path('languages/', views.languages),
 
 ]
--- MainApp/views.py
+++ MainApp/views.py
@@ -1,11 +1,45 @@
 from django.shortcuts import render, HttpResponse
-
+import json
 # Create your views here.
 def home(request):
-    text = """
-    <h1><b>"Hello World!" Это задание из Модуля 1</b></h1>
-    <i><u>Автор:</u> Никита Тимофеев</i> 
-    """
-    return HttpResponse(text)
+    return render(request, 'index.html')
+    #text = """
+    #<h1><b>"Hello World!" Это задание из Модуля 1</b></h1>
+    #<i><u>Автор:</u> Никита Тимофеев</i>
+    #"""
+    #return HttpResponse(text)
 def about(request):
     return HttpResponse("Никита Тимофеев")
+
+with open("countries.json", "r") as f:
+    countries = json.load(f)
+
+with open("countries.json", "r") as f:
+    language = json.load(f)
+
+def countries_list(request):
+    context = {'countries': countries}
+    return render(request, 'countries_list.html', context)
+
+def languages(request):
+    context = {'language': language}
+    return render(request, 'languages.html', context)
+
+def country_page(request, id):
+    for country in countries:
+        if country['id'] == id:
+            context = {
+                'country': country
+            }
+            return HttpResponse("тест")
+
+            #return render(request, 'country_page.html', context)
+
+            #text = f"""<h2>{country['country']}</h2>
+            #язык: {languages['languages']}
+            #"""
+            #return HttpResponse(text)
+            #context = {
+                #'item': item
+            #}
+            #return render(request, 'item-page.html', context)
